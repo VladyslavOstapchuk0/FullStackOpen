ff2bf187a19d0667b4fbd866949a3e3654775e81
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-07-28 23:01:36 +0300

    Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,19 +2,34 @@ import { useState } from 'react';
 
 const Button = ({ handleClick, text }) => {
   return (
-    <>
+    <div>
       <button onClick={handleClick}>{text}</button>
-    </>
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad;
+  let average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all;
+  let positive = all === 0 ? 0 : (good / all) * 100;
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
   );
 };
 
 const App = () => {
+  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let all = good + neutral + bad;
-  let average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all;
-  let positive = all === 0 ? 0 : (good / all) * 100;
   const handleClickGood = () => {
     setGood(good + 1);
   };
@@ -33,13 +48,7 @@ const App = () => {
       <Button handleClick={() => handleClickGood()} text="good" />
       <Button handleClick={() => handleClickNeutral()} text="neutral" />
       <Button handleClick={() => handleClickBad()} text="bad" />
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
