ffa8da55000bad4f551fcb6ff3c3ac7b39555d12
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-07-29 00:30:43 +0300

    Exercise 2.4

--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -5,6 +5,7 @@ import Content from './Content';
 const Course = ({ course }) => {
   return (
     <>
+      <h2>Web development curriculum</h2>
       <Header name={course.name} />
       <Content parts={course.parts} />
     </>
