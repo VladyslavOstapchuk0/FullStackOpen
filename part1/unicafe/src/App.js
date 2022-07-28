e441186ea77a2afcb875bdc05020e79435c7319b
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-07-28 22:56:47 +0300

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,11 +9,12 @@ const Button = ({ handleClick, text }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  let all = good + neutral + bad;
+  let average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all;
+  let positive = all === 0 ? 0 : (good / all) * 100;
   const handleClickGood = () => {
     setGood(good + 1);
   };
@@ -36,6 +37,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   );
 };
