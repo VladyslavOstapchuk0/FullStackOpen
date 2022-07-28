dbdb14b7a29b7a72398e56561a48cd827addb6be
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-07-29 01:15:13 +0300

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   const handleAddName = (event) => {
     event.preventDefault();
     const newNameAdd = {
       name: newName,
       number: newNumber,
+      id: persons[persons.length - 1].id + 1,
     };
     if (persons.some((e) => e.name === newName)) {
       alert(`${newName} is already added to the phonebook`);
@@ -29,9 +40,17 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const onFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with a: <input value={filter} onChange={onFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleAddName}>
         <div>
           name: <input value={newName} onChange={onNameChange} />
@@ -45,9 +64,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map((person) => {
+        {filteredPersons.map((person) => {
           return (
-            <p key={person.name}>
+            <p key={person.id}>
               {person.name} {person.number}
             </p>
           );
