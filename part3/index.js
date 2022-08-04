8eb6e4b8e313c2b51ceaf7efe7718cb8843d5ed8
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-08-04 23:12:52 +0300

    heroku

--- part3/index.js
+++ part3/index.js
@@ -14,6 +14,7 @@ morgan.token('body', (req, res) => {
 });
 
 app.use(express.json());
+app.use(express.static('build'));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
