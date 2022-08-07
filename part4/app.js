b28dd235ca384c475c5ee6bff1bda199c04b2df0
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-08-07 05:42:56 +0300

    Exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -10,6 +10,7 @@ const logger = require('./utils/logger');
 
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 logger.info('connecting to db...');
 
@@ -29,6 +30,7 @@ app.use(middleware.reqLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
